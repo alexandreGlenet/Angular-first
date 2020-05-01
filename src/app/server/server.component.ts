c1179afc6c964a7093299ea4456e49ae3cc707e3
Alexandre Glenet <alexandreglenet30@gmail.com>
2020-05-01 14:00:21 +0200

    Création du 1er Composant
    Création d'un dossier server, avec dedans mon premier composant.

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'app-server', // Ici je crée mon sélécteur que je pourrai utiliser dans un template.
+  templateUrl: './server.component.html'
+})
+export class serverComponent {
+
+}

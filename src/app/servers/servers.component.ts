2de24cf9795d34abb4c094e3d2bec47b5d3ac129
Alexandre Glenet <alexandreglenet30@gmail.com>
2020-05-01 17:14:52 +0200

    Précision sur les sélecteurs.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // selector: '[app-servers]',
-  // selector: '.app-servers',
+  // selector: '[app-servers]', // Permet de transformer le seleceteur en attribut et de l'appeler dans une div et plus en balise.<div app-server>
+  // selector: '.app-servers', // Permet de changer le selecteur pour l'appeler comme une classe class="app-servers"
   selector: 'app-servers',
   template: `
     <app-server></app-server>

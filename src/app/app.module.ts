fedb1b92df968b1dbb5b72b4d5b1dff4a6de75da
Alexandre Glenet <alexandreglenet30@gmail.com>
2020-05-01 13:06:11 +0200

    Intro - ngModel
    Petite intro douce au ngModel, avec importation
    dans module app.module.ts, du module '@angular/forms'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+// app.module est le fichier qui me permet d'indiquer à Angular tous les modules dont on va se servir.
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
